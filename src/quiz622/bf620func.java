b04e6438109c0857050f27ff46a039770eb0aa10
dog7350 <dog7350@gmail.com>
2023-06-22 11:31:45 +0900

    FinTech Study 0622 - BF

---
+++ src/quiz622/bf620func.java
@@ -0,0 +1,44 @@
+package quiz622;
+
+import static prt.ObjectPrint.*;
+import java.util.*;
+
+public class bf620func {
+    public String name, grade;
+    public int kor, eng, math, sum;
+    public double avg;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void insert() {
+        prt("\n이름 입력 : ");
+        name = sc.next();
+        prt("\n국어 성적 : ");
+        kor = sc.nextInt();
+        prt("\n영어 성적 : ");
+        eng = sc.nextInt();
+        prt("\n수학 성적 : ");
+        math = sc.nextInt();
+    }
+
+    public void exec() {
+        sum = kor + eng + math;
+        avg = (double) sum / 3;
+        prt("\n연산 완료");
+    }
+
+    public void funPrint() {
+        prt("\n이름 : " + name);
+        prt("\n국어 : " + kor);
+        prt("\n영어 : " + eng);
+        prt("\n수학 : " + math);
+        prt("\n총합 : " + sum);
+        prt("\n평균 : " + avg);
+        prt("\n성적 : " + grade);
+    }
+
+    public void grade() {
+        grade = ((eng >= 90) ? "A" : (eng >= 80) ? "B" : "C");
+        prt("\n등급 완료");
+    }
+}
---
+++ src/quiz622/bf620main.java
@@ -0,0 +1,40 @@
+package quiz622;
+
+import static prt.ObjectPrint.*;
+import java.util.*;
+
+public class bf620main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        bf620func bf = new bf620func();
+        int cmd = 0;
+        boolean flag = true;
+
+        while (flag) {
+            prt("\n1. 입력\n2. 연산\n3. 출력\n4. 등급\n5. 프로그램 종료\n명령어 입력 >> ");
+            cmd = sc.nextInt();
+
+            switch (cmd) {
+                case 1 :
+                    bf.insert();
+                    break;
+                case 2 :
+                    bf.exec();
+                    break;
+                case 3 :
+                    bf.funPrint();
+                    break;
+                case 4 :
+                    bf.grade();
+                    break;
+                case 5 :
+                    flag = false;
+                    prt("\n프로그램 종료\n");
+                    break;
+                default :
+                    prt("\n존재하지 않는 명령어입니다.\n");
+            }
+        }
+    }
+}

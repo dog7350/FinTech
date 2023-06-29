493215c51f6f85b733baa96e392412b0c3919529
dog7350 <dog7350@gmail.com>
2023-06-29 10:50:23 +0900

    FinTech Study 0629 - Full

---
+++ src/study/s629/generic01.java
@@ -0,0 +1,16 @@
+package study.s629;
+
+class genericClass01< T > {
+    public void sumFunc(T n1, T n2) {
+        System.out.println(n1 + ", " + n2);
+    }
+}
+
+public class generic01 {
+    public static void main(String[] args) {
+        genericClass01<Integer> g1 = new genericClass01();
+        g1.sumFunc(10, 20);
+        genericClass01<String> g2 = new genericClass01();
+        g2.sumFunc("aaa", "bbb");
+    }
+}

459e4de91395ec2d9cdf54b69f227aca0de50881
dog7350 <dog7350@gmail.com>
2023-07-20 09:14:08 +0900

    FinTech Study 0719 - LC_Full

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ import java.util.*;
 public class Main extends ObjectPrint {
     public static void main(String args[]) {
         ObjectPrint o = new ObjectPrint();
-        /*  ★ 현재까지 수업 내용 ★
-            아스키 코드, 변수, IF, Switch, 3항, While, For, 배열, ArrayList, Iterator, HashSet, Map, String, Random
-            객체, Class, Method, Single Tone, Thread.sleep, currentTimeMillis, SimpleDateFormat, Date, tryCatch, Thread     */
         Scanner sc = new Scanner(System.in);
 
-
+        int[] apple = {1, 2, 3, 1, 2, 3, 1};
+        int k = 3, m = 4;
+        int[] score;
 
     }
 }
